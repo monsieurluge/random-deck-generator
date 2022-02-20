f57213de8e6049ad24cf10bfed683f477d7e90b1
monsieurluge <monsieurluge@gmail.com>
2022-02-20 16:23:51 +0100

    add booster draft generator informations

--- src/plugins/mtg/generator/booster-draft-generator.ts
+++ src/plugins/mtg/generator/booster-draft-generator.ts
@@ -6,6 +6,35 @@ import { MtgCard } from '../card/card'
 import { and, colorIs, Constraint, not, ofType } from '../constraints'
 import { MtgDeck } from '../deck'
 
+/**
+ * simulates a 8 player draft, with set boosters
+ * cards distribution in a set (https://cardgamebase.com/mtg-set-booster-contents):
+ * welcome
+ *   - 35% 5C, 1U
+ *   - 40% 4C, 2U
+ *   - 12.5% 3C, 3U
+ *   - 7% 2C, 4U
+ *   - 3.5% 1C, 5U
+ *   - 2% 6U
+ * fireworks 1
+ *   - 50% 1C
+ *   - 50% 1U
+ * fireworks 2
+ *   - 49% 2C
+ *   - 24.5% 1C, 1U
+ *   - 17.5% 1C, 1R / MR
+ *   - 3.1% 2U
+ *   - 4.3% 1U, 1R / MR
+ *   - 1,6% 2R / MR
+ * big finish 1
+ *   - 86,5% R
+ *   - 13,5% MR
+ * big finish 2
+ *   - 71,4% C
+ *   - 21,4% U
+ *   - 6,2% R
+ *   - 1,0% MR
+ */
 export function BoosterDraftGenerator(collection: CardCollection): Generator<MtgCard> {
     const whiteCardNotLand: Constraint = and([
         colorIs('white'),
